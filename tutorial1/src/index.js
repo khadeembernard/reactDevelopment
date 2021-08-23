ecc8086dc0f9a32adaf197e524614169013f1298
Koro Sensei <51177688+khadeembernard@users.noreply.github.com>
2021-08-23 16:11:48 -0700

    Day 1: Completed the basic environment setup and begun tutorial

---
+++ tutorial1/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM  from 'react-dom';
+
+
+function Greeting(){
+  return <h1>Welcome to my Techno Disco</h1>;
+}
+
+ReactDOM.render(<Greeting/>,document.getElementById('root'));
