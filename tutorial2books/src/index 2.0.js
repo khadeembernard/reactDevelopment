616e71ed9411cd41f34d49e8f68b8d930342e25e
Koro Sensei <51177688+khadeembernard@users.noreply.github.com>
2021-08-26 11:32:24 -0700

    files were lost had restart from last commit, caught up and excluded node modles in .gitIgnore

--- tutorial2books/src/index 2.0.js
+++ tutorial2books/src/index 2.0.js
@@ -3,26 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function BookList() {
+  //passing paremeters to components
   return(
     <section className='bookList'>
-      <Book/>
-      <Book/>
+      <Book job='developer' title='The Value Of ES6'/>
+      <Book pages={125}/>
       <Book/>
       <Book/>
     </section>
   );
 }
 const author ='Eric Mathhes'
-const Book = ()=>{
+//you can call your input parameter what you want but props is a
+  //a reserved word and an industry standard to use. It will also make your components easier to reuse JUST MAKE SURE TO COMMENT 
+const Book = (props)=>{
+  //props is an object
+  console.log(props)
   const title='Python Crash Course, 2nd Edition'
   const img='https://images-na.ssl-images-amazon.com/images/I/81vmJCNCm6L._AC_UL200_SR200,200_.jpg'
   return <article className='book'>
    <img src={img} alt={title} />
    <h1>{title}</h1>
    <h3>{author.toUpperCase}</h3>
+   <h4>{props.job}</h4>
+   <h4>{props.pages}</h4>
    {/* <p>{let x=6</p> */}
    {/* <p>{24+5}</p> */}
   </article>
 }
+//notice what renders for props...
 
 ReactDOM.render(<BookList/>,document.getElementById('root'))
